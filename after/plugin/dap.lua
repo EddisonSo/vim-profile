fd1886a85770c18ddbb7f4c6f80be1dbcc230853
EddisonSo <eddisonso@gmail.com>
2024-03-25 12:35:48 -0400

    added dap

---
+++ after/plugin/dap.lua
@@ -0,0 +1,11 @@
+require("dapui").setup()
+
+vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end)
+vim.keymap.set('n', '<Leader>do', function() require('dap').step_over() end)
+vim.keymap.set('n', '<Leader>di', function() require('dap').step_into() end)
+vim.keymap.set('n', '<Leader>dt', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dB', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set('n', '<Leader>du', function() require('dapui').toggle() end)
--- lua/required/packer.lua
+++ lua/required/packer.lua
@@ -61,4 +61,8 @@ return require('packer').startup(function(use)
 
   use 'nvim-tree/nvim-web-devicons'
 
+  use 'mfussenegger/nvim-dap'
+
+  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+
 end)
